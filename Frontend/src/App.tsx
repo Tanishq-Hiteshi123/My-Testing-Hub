19d344cd41f58d9a12c8d0547f757020a657eaf2
Tanishq-Hiteshi123 <tanishq.yadav@hiteshi.com>
2025-01-14 10:18:54 +0530

    Starting with adding coding Feature

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,42 +1,89 @@
-
-import { lazy, Suspense } from 'react'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
-import PrivateRouter from './myComponents/PrivateRouter'
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRouter from "./myComponents/PrivateRouter";
 function App() {
-
-  const AuthPageComponent = lazy(() => import("./pages/AuthPage"))
-  const StudentDashboardComponent = lazy(() => import("./pages/StudentDashboard"))
-  const AdminDashboardComponent = lazy(() => import("./pages/AdminDashboard"))
-  const FacultyDashboardComponent = lazy(() => import("./pages/FacultyDashboard"))
-  const McqTestPageComponent = lazy(() => import("./pages/MCQTestPage"))
+  const AuthPageComponent = lazy(() => import("./pages/AuthPage"));
+  const StudentDashboardComponent = lazy(
+    () => import("./pages/StudentDashboard")
+  );
+  const AdminDashboardComponent = lazy(() => import("./pages/AdminDashboard"));
+  const FacultyDashboardComponent = lazy(
+    () => import("./pages/FacultyDashboard")
+  );
+  const McqTestPageComponent = lazy(() => import("./pages/MCQTestPage"));
   return (
-   <>
-     <Router>
-        <Suspense fallback = {<>Loading...</>}>
-      <Routes>
-        <Route element = {<PrivateRouter></PrivateRouter>}>
-        <Route path='/' element = {<AuthPageComponent />} />
-        <Route path='/studentDashboard' element = {<StudentDashboardComponent />} />
-        <Route path="/studentDashboard" element={<StudentDashboardComponent />} />
-        <Route path="/studentDashboard/profile" element={<StudentDashboardComponent />} />
-        <Route path="/studentDashboard/MCQActiveTest" element={<StudentDashboardComponent />} />
-        <Route path="/studentDashboard/MCQCompletedTest" element={<StudentDashboardComponent />} />
-        <Route path="/studentDashboard/MCQUpcomingTest" element={<StudentDashboardComponent />} />
-        <Route path="/studentDashboard/progress" element={<StudentDashboardComponent />} />
-        <Route path='/adminDashboard' element = {<AdminDashboardComponent />} />
-        <Route path='/facultyDashboard' element = {<FacultyDashboardComponent />} />
-        <Route path='/facultyDashboard/addCodingQuestion' element = {<FacultyDashboardComponent />} />
-        <Route path='/facultyDashboard/createCodingTest' element = {<FacultyDashboardComponent />} />
-        <Route path='/facultyDashboard/addMCQQuestion' element = {<FacultyDashboardComponent />} />
-        <Route path='/facultyDashboard/createMCQTest' element = {<FacultyDashboardComponent />} />
-         
-        <Route path='/mcqTestPage/:testId' element = {<McqTestPageComponent/>}/>
-        </Route>
-      </Routes>
+    <>
+      <Router>
+        <Suspense fallback={<>Loading...</>}>
+          <Routes>
+            <Route element={<PrivateRouter></PrivateRouter>}>
+              <Route path="/" element={<AuthPageComponent />} />
+              <Route
+                path="/studentDashboard"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/studentDashboard"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/studentDashboard/profile"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/studentDashboard/MCQActiveTest"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/studentDashboard/MCQCompletedTest"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/studentDashboard/MCQUpcomingTest"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/studentDashboard/progress"
+                element={<StudentDashboardComponent />}
+              />
+              <Route
+                path="/adminDashboard"
+                element={<AdminDashboardComponent />}
+              />
+              <Route
+                path="/facultyDashboard"
+                element={<FacultyDashboardComponent />}
+              />
+              <Route
+                path="/facultyDashboard/addCodingQuestion"
+                element={<FacultyDashboardComponent />}
+              />
+              <Route
+                path="/facultyDashboard/createCodingTest"
+                element={<FacultyDashboardComponent />}
+              />
+              <Route
+                path="/facultyDashboard/addMCQQuestion"
+                element={<FacultyDashboardComponent />}
+              />
+              <Route
+                path="/facultyDashboard/createMCQTest"
+                element={<FacultyDashboardComponent />}
+              />
+
+              <Route
+                path="/mcqTestPage/:testId"
+                element={<McqTestPageComponent />}
+              />
+
+              {/* Routes for the coding test Problems */}
+              <Route path="/coding/:testId" element={} />
+            </Route>
+          </Routes>
         </Suspense>
-     </Router>
-   </>
-  )
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/pages/CodingTestPage.tsx
@@ -0,0 +1,9 @@
+function CodingTestPage() {
+  return (
+    <div>
+      <h1>This is the coding Test Platform</h1>
+    </div>
+  );
+}
+
+export default CodingTestPage;
